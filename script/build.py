b62475b7574c137e24cc4cd759e7b3954929825f
Nikita Prokopov <niki@tonsky.me>
2022-01-13 21:13:55 +0100

    test on build

--- script/build.py
+++ script/build.py
@@ -1,5 +1,17 @@
 #! /usr/bin/env python3
-import build_utils, common, glob, os, sys
+import build_utils, common, glob, os, subprocess, sys
+
+def test():
+  classpath = [
+    *common.deps('-clojure'),
+    "target/classes-clojure",
+    "test-clojure",
+  ]
+  subprocess.check_call(["java",
+    "--class-path", build_utils.classpath_join(classpath),
+    "clojure.main",
+    "-m", "io.github.humbleui.test"])
+  return 0
 
 def main(classifier=''):
   os.chdir(common.basedir)
@@ -8,6 +20,8 @@ def main(classifier=''):
                     classpath=common.deps(classifier),
                     modulepath=common.deps(classifier),
                     release='9')
+  if '-clojure' == classifier:
+    test()
   return 0
 
 if __name__ == '__main__':
